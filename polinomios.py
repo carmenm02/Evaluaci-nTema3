17fb1d1f8c12294cb5a137164cbc9fff1e673bd6
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-10-28 17:01:00 +0200

    con esta funcion he separado el polinomio en terminos

--- polinomios.py
+++ polinomios.py
@@ -6,3 +6,10 @@ def restar(p1,p2):
     return p1 - p2
 def dividir(p1,p2):
     return p1 / p2
+
+def eliminar(polinomio):
+    polinomio = input("Introduce un polinomio: ")
+    terminos = separarterminos(polinomio)
+    for i in terminos:
+        print(i)
+       
