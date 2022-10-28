502379e1e8c261d021f955ca687d27de70559d36
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-10-28 15:56:40 +0200

    he calculado el determinante

--- regladesarrus.py
+++ regladesarrus.py
@@ -15,3 +15,5 @@ a12 = float(input("Ingresa el valor de a12: "))
 a20 = float(input("Ingresa el valor de a20: "))
 a21 = float(input("Ingresa el valor de a21: "))
 a22 = float(input("Ingresa el valor de a22: "))
+
+total = ((a00*a11*a22) + (a10*a21*a02) + (a20 * a01 * a12)) - ((a20*a11*a02)+(a00*a21*a22)+(a10*a01*a22))
