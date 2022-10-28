fd71963d4ce6349603e5230fe03b95d3cface25b
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-10-28 15:45:28 +0200

    he creado la matriz

---
+++ regladesarrus.py
@@ -0,0 +1,17 @@
+from curses import raw
+
+
+print("Ingresa los valores")
+print("|a00 a01 a02|")
+print("|a10 a11 a12|")
+print("|a20 a21 a22")
+
+a00 = float(input("Ingresa el valor de a00: "))
+a01 = float(input("Ingresa el valor de a01: "))
+a02 = float(input("Ingresa el valor de a02: "))
+a10 = float(input("Ingresa el valor de a10: "))
+a11 = float(input("Ingresa el valor de a11: "))
+a12 = float(input("Ingresa el valor de a12: "))
+a20 = float(input("Ingresa el valor de a20: "))
+a21 = float(input("Ingresa el valor de a21: "))
+a22 = float(input("Ingresa el valor de a22: "))
---
+++ torredehanoi.py
@@ -0,0 +1,4 @@
+def Torre:
+    disco = 0
+    if disco >= 0 and disco <= 74:
+        
