51c988bd242404945de5b2c60e3e5ed6ba032a17
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-10-28 16:00:05 +0200

    al ejecutar cada numero me sale con .0 al final

--- regladesarrus.py
+++ regladesarrus.py
@@ -17,3 +17,11 @@ a21 = float(input("Ingresa el valor de a21: "))
 a22 = float(input("Ingresa el valor de a22: "))
 
 total = ((a00*a11*a22) + (a10*a21*a02) + (a20 * a01 * a12)) - ((a20*a11*a02)+(a00*a21*a22)+(a10*a01*a22))
+
+if total != 0:
+    print(a00," ",a01," ",a02)
+    print(a10," ",a11," ",a12)
+    print(a20," ",a21," ",a22)
+    print("Determinante: ",total)
+else:
+    print("Determinante igual a 0")
